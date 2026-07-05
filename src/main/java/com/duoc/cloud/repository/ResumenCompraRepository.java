77a6c84b67acadfcd1d732908439b7e2e7ef0d78
JenniferGonzalezT <je.gonzalezt@duocuc.cl>
2026-07-05 18:53:36 -0400

    S7: Clase Repositorio ResumenCompra

---
+++ src/main/java/com/duoc/cloud/repository/ResumenCompraRepository.java
@@ -0,0 +1,7 @@
+package com.duoc.cloud.repository;
+
+import com.duoc.cloud.model.ResumenCompra;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumenCompraRepository extends JpaRepository<ResumenCompra, Long> {
+}
